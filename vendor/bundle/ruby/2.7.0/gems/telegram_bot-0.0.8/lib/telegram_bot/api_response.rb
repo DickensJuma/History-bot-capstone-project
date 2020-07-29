119b75ada6aca704e3494628ab2a0ea9878a794b
DickensJuma <dickensjuma13@gmail.com>
2020-07-29 14:21:42 +0300

    setup rspec

---
+++ vendor/bundle/ruby/2.7.0/gems/telegram_bot-0.0.8/lib/telegram_bot/api_response.rb
@@ -0,0 +1,20 @@
+module TelegramBot
+  class ApiResponse
+    attr_reader :body, :ok, :result
+
+    def initialize(res)
+      @body = res.body
+      if res.status == 200
+        data = JSON.parse(body)
+        @ok = data["ok"]
+        @result = data["result"]
+      else
+        @ok = false
+        error = ResponseError.new(res)
+        fail error, "An error has occurred: #{error.data}", caller
+      end
+    end
+
+    alias_method :ok?, :ok
+  end
+end
